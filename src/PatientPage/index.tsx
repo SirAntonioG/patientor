33d58187775cac643f61a970ad53703614774554
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-28 14:01:32 -0500

    fix: icon gender

--- src/PatientPage/index.tsx
+++ src/PatientPage/index.tsx
@@ -1,24 +1,42 @@
 import { Box, Typography } from '@material-ui/core';
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
 import { useParams } from 'react-router-dom';
 
 import { useStateValue } from '../state';
+import { Patient } from '../types';
 
 const PatientPage = () => {
   const { id } = useParams<{ id: string }>();
   const [{ patients }] = useStateValue();
 
-  const patient = Object.values(patients).find((p) => p.id === id);
+  const patient = Object.values(patients).find((p: Patient) => p.id === id);
+
+  if (patient === undefined) {
+    return (
+      <Box>
+        <Typography variant='h2' style={{ marginBottom: '0.5em' }}>
+          Patient not found
+        </Typography>
+      </Box>
+    );
+  }
 
   return (
     <div className='App'>
       <Box>
         <Typography
           variant='h3'
-          component='h1'
+          component='h2'
           style={{ marginBottom: '0.5em' }}
         >
-          {patient.name}
+          {patient.name}{' '}
+          {patient.gender === 'female' ? <FemaleIcon /> : <MaleIcon />}
         </Typography>
+        <p>
+          ssn: {patient.ssn} <br />
+          occupation: {patient.occupation}
+        </p>
       </Box>
     </div>
   );
